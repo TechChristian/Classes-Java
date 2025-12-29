a67c25838465635945256266b58d3bcfae097ee5
Christian Lopes <christianx111.ls@gmail.com>
2025-12-28 23:30:04 -0300

    Metodo de aplicar Aumento Adicionado

--- src/Car.java
+++ src/Car.java
@@ -1,37 +1,56 @@
 public class Car {
 
     // Encapsulation
-    private  String make;
-    private  String model;
-    private  int year;
+    private String make;
+    private String model;
+    private int year;
+    private double value;
 
     // attributes
-    Car(String make, String model, int year){
+    Car(String make, String model, int year, double value) {
         this.make = make;
         this.model = model;
         this.year = year;
+        this.value = value;
+    }
+
+    // method to apply increase
+    public void applyIncrease(double percentage) {
+        this.value += this.value * percentage;
     }
 
     // getters (Access the attributes securely.)
-    public String getMake(){
+    public String getMake() {
         return make;
     }
-    public String getModel(){
+
+    public String getModel() {
         return model;
     }
-    public int getYear(){
+
+    public int getYear() {
         return year;
     }
 
+    public double getValue() {
+        return value;
+    }
+
     // setters (Modify the attributes securely.)
-    public void setMake(String make){
+    public void setMake(String make) {
         this.make = make;
     }
-    public void setModel(String model){
+
+    public void setModel(String model) {
         this.model = model;
     }
-    public void setYear(int year){
+
+    public void setYear(int year) {
         this.year = year;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args){
-        Car car = new Car("BMW", "X6", 2024);
+        Car car = new Car("BMW", "X6", 2024, 125000 );
 
-        car.setMake("Ford");
-        car.setModel("KA");
-        car.setYear(2015);
+        car.applyIncrease(0.10);
 
         // Accessing attributes via getters
         System.out.println(car.getMake());
         System.out.println(car.getModel());
         System.out.println(car.getYear());
+        System.out.println("Valor do Carro : " + car.getValue());
     }
 }
